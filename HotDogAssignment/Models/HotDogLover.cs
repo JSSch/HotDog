f18b32a1d0f515809350cd0d304365c14906e0a0
JSSch <smartblue@gmail.com>
2015-03-25 15:28:27 -0400

    Commit 6
    Messed with data annotations to get date to display correctly, whew!

--- HotDogAssignment/Models/HotDogLover.cs
+++ HotDogAssignment/Models/HotDogLover.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,6 +11,8 @@ namespace HotDogAssignment.Models
         public string ProfilePic { get; set; }
         public string FavoriteDog { get; set; }
         public string LastDog { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode=true)]
         public DateTime Date { get; set; }
         public string Name { get; set; }
         public string Bio { get; set; }
